beb990340ad3a2df630d27c636ad67f6c3d2ab71
CragglesG <121510295+CragglesG@users.noreply.github.com>
2025-03-11 19:45:22 +0000

    Update website to add radar and remove login animation, switch to bun, and move pyproject.toml

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,11 +1,10 @@
-import "./css/index.css"
-import Terminal from './js/Terminal';
+import "./css/index.css";
+import Terminal from "./js/Terminal";
 
 export default function App() {
-    return (
-        <div>
-            
-            <Terminal />
-        </div>
-    );
+  return (
+    <div>
+      <Terminal />
+    </div>
+  );
 }
