e011af742ecd78a15911daa69953dc5a571114b6
Craig Giles <121510295+CragglesG@users.noreply.github.com>
2025-06-26 21:14:41 +0000

    Add a space between the format and function
    Add

--- website/src/js/IdeaGen.jsx
+++ website/src/js/IdeaGen.jsx
@@ -18,7 +18,7 @@ function generateIdea() {
   const r1 = Math.floor(Math.random() * (4 + 1));
   const r2 = Math.floor(Math.random() * (4 + 1));
 
-  return formats[r1] + functions[r2];
+  return formats[r1] + " " + functions[r2];
 }
 
 export default function IdeaGen() {
