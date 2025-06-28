34690da23632226f8a3a7d163db3909728725888
CragglesG <121510295+CragglesG@users.noreply.github.com>
2025-06-28 13:46:28 +0100

    Add APRS

--- website/src/js/IdeaGen.jsx
+++ website/src/js/IdeaGen.jsx
@@ -3,20 +3,21 @@ const formats = [
   "a terminal app",
   "a desktop app",
   "a Python program",
-  "a Slack interface",
+  "a Slack bot",
 ];
 
 const functions = [
   "for viewing pictures from weather satellites! (hint: check out Automatic Picture Transmission)",
-  "for tracking ships in real-time! (hint: you might want to use the Automatic Identification System for this)",
+  "for tracking ships in real-time! (hint: you might want to use Automatic Identification System for this)",
   "for tracking planes in real-time! (hint: take a look at ADS-B)",
   "for listening to local radio stations!",
   "for viewing images from the ISS! (hint: these are transmitted using SSTV)",
+  "for receiving data from HAM radio stations! (hint: APRS might be helpful here)",
 ];
 
 function generateIdea() {
   const r1 = Math.floor(Math.random() * (4 + 1));
-  const r2 = Math.floor(Math.random() * (4 + 1));
+  const r2 = Math.floor(Math.random() * (5 + 1));
 
   return formats[r1] + functions[r2];
 }
